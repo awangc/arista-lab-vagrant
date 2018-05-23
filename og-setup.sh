253a83e0d1ed7b73c668b2df442d4bb337b22bce
Andrew Wang <andrew_wang@cable.comcast.com>
2018-05-23 16:37:49 -0400

    Set nexthop for anycast routes to the og ip

--- og-setup.sh
+++ og-setup.sh
@@ -17,11 +17,29 @@ echo "vm.nr_hugepages = 1024" >> /etc/sysctl.conf
 mkdir -p /mnt/huge && mount -t hugetlbfs nodev /mnt/huge
 echo "hugetlbfs /mnt/huge hugetlbfs rw,mode=0777 0 0" >> /etc/fstab
 
+# disable reverse path filtering, so e.g. a host can reply to ping packets for
+# destination address A it receives on an interface bound to address != A
+#
+# this setting usually is disabled per interface, we use all and default
+# to disable for all interfaces. See this email for how 'all' and 'default'
+# and 'rp_filter' work together: https://marc.info/?l=linux-kernel&m=123606366021995&w=2
+echo "0" > /proc/sys/net/ipv4/conf/all/rp_filter
+echo "0" > /proc/sys/net/ipv4/conf/default/rp_filter
+echo "0" > /proc/sys/net/ipv4/conf/enp0s8/rp_filter
+echo "0" > /proc/sys/net/ipv4/conf/enp0s9/rp_filter
+echo "net.ipv4.conf.default.rp_filter = 0" >> /etc/sysctl.conf
+echo "net.ipv4.conf.all.rp_filter = 0" >> /etc/sysctl.conf
+echo "net.ipv4.conf.enp0s8.rp_filter = 0" >> /etc/sysctl.conf
+
+# enable ip forwarding, so packets can be passed to different interfaces
+echo "1" > /proc/sys/net/ipv4/ip_forward
+echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
+
 # Install the uio_pci_generic driver
-# modprobe uio_pci_generic
+modprobe uio_pci_generic
 
 # Load modules at boot
-#echo "uio" >> /etc/modules
-#echo "uio_pci_generic" >> /etc/modules
+echo "uio" >> /etc/modules
+echo "uio_pci_generic" >> /etc/modules
 
 echo "cd /vagrant" >> /home/vagrant/.bashrc
